f90bb205fb054b41bafcd776d002cd5f6b937040
Harun KARATAS <harun_karatash@outlook.com>
2019-01-04 23:52:42 +0300

    2. ders sonu

---
+++ Client/CommandOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Copier
+{
+    public class CommandOptions
+    {
+        [Option('s', "sourceDirectoryPath", HelpText = "Parent directory where the files will be. If skipped current directory will be searched.")]
+        public string SourceDirectoryPath { get; set; }
+
+        [Option('f', "fileGlobPattern", Required = true, HelpText = "Files to be searched. Accepts glob pattern for pattern matching.")]
+        public string FileGlobPattern { get; set; }
+
+        [Option('d', "destinationDirectoryPath", Required = true, HelpText = "Destination directory path")]
+        public string DestinationDirectoryPath { get; set; }
+
+        [Usage]
+        public static IEnumerable<Example> Examples => new List<Example>()
+        {
+            new Example( "Starts the copier",new UnParserSettings{PreferShortName = true},
+            new CommandOptions
+            {
+                SourceDirectoryPath = "/Users/harun/Desktop",
+                FileGlobPattern = "*.jpg",
+                DestinationDirectoryPath = "/Users/harun/Desktop/Cambridge_English_Vocabulary_ in_Use/Tools"
+
+            })
+        };
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
+using Copier;
 
 namespace Client
 {
@@ -6,7 +9,47 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Parser.Default.ParseArguments<CommandOptions> (args)
+              .WithParsed(StartWatching)
+              .WithNotParsed(a =>
+              {
+                  Environment.Exit(1);
+              });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
+
+        private static void StartWatching(CommandOptions options)
+        {
+            Console.WriteLine("Watching has started");
+            WatchFile(options.FileGlobPattern, options.SourceDirectoryPath);
+        }
+
+        private static void WatchFile(String filePattern, String sourceDirectoryPath)
+        {
+            var watcher = new FileSystemWatcher
+            {
+                Path = sourceDirectoryPath,
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                Filter = filePattern
+            };
+
+            watcher.Changed += (sender, args) =>
+            {
+                if (args.ChangeType == WatcherChangeTypes.Changed)
+                {
+                    Console.WriteLine($"{args.Name} File has changed{args.ChangeType}");
+                }
+            };
+            watcher.Renamed += (sender, args) => Console.WriteLine($"{args.OldName} File has been renamed to {args.Name}");
+
+            //Start watching the file
+            watcher.EnableRaisingEvents = true;
+        }
+
+
+     
     }
 }
