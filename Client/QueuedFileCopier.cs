96d1959e2dc6b6e9d4755ee0758c63040502b547
Harun KARATAS <harun_karatash@outlook.com>
2019-01-10 21:50:58 +0300

    Added new logic with timer

--- Client/QueuedFileCopier.cs
+++ Client/QueuedFileCopier.cs
@@ -3,6 +3,15 @@ namespace Copier
 {
     public class QueuedFileCopier : IFileCopier
     {
+        private CommandOptions options;
+        private ILogger logger;
+
+        public QueuedFileCopier(CommandOptions options, ILogger logger)
+        {
+            this.options = options;
+            this.logger = logger;
+        }
+
         public void CopyFile(CommandOptions options, string fileName)
         {
             throw new NotImplementedException();
