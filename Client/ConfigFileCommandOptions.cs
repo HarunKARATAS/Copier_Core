02aaa8ea2175139d02ba8eb48d0228425c5292f8
Harun KARATAS <harun_karatash@outlook.com>
2019-01-11 11:08:39 +0300

    Added experimenting with config file

---
+++ Client/ConfigFileCommandOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommandLine;
+
+namespace Copier
+{
+    public class ConfigFileCommandOptions
+    {
+        [Option('c',"configFilePath",Default = "config.txt",HelpText = "Path of the configuration file which can be used instead of passing all the options to the command line.")]
+        public string ConfigFilePath { get; set; }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -10,8 +10,9 @@ namespace Client
         static void Main(string[] args)
         {
 
-            Parser.Default.ParseArguments<CommandOptions> (args)
-              .WithParsed(StartWatching)
+            Parser.Default.ParseArguments<CommandOptions, ConfigFileCommandOptions> (args)
+              .WithParsed<CommandOptions>(StartWatching)
+              .WithParsed<ConfigFileCommandOptions>(StartWatchingWithConfigurationFile)
               .WithNotParsed(a =>
               {
                   Environment.Exit(1);
@@ -19,6 +20,28 @@ namespace Client
 
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
+        }
+
+        private static void StartWatchingWithConfigurationFile(ConfigFileCommandOptions options)
+        {
+
+            ILogger logger = new ConsoleLoggger();
+            if (File.Exists(options.ConfigFilePath))
+            {
+                var configContent = File.ReadAllLines(options.ConfigFilePath);
+                var commandOptions = string.Join(" ", configContent);
+                Parser.Default.ParseArguments<CommandOptions>(configContent)
+                    .WithParsed(StartWatching)
+                    .WithNotParsed(a =>
+                    {
+                        logger.LogError("Configuration file does not parsed correctly.");
+                        Environment.Exit(1);
+                    });
+            }
+            else
+            {
+                logger.LogError($"Configuration file does not exist in {options.ConfigFilePath} path");
+            }
         }
 
         private static void StartWatching(CommandOptions options)
